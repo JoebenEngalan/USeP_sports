9f4ca88d83484b29352ed339bfb13565cee0c83a
trone1993@gmail.com <trone1993@gmail.com>
2019-07-01 10:23:17 +0800

    format change

--- templates/scripts.php
+++ templates/scripts.php
@@ -1,4 +1,4 @@
-<!-- Bootstrap core JavaScript-->
+`   <!-- Bootstrap core JavaScript-->
     <script src="vendor/jquery/jquery.min.js"></script>
     <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
