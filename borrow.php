569ccba0f25181623f6d3bc9d2eeab15f9ec2fa9
trone1993@gmail.com <trone1993@gmail.com>
2019-07-01 12:04:41 +0800

    commit change

--- borrow.php
+++ borrow.php
@@ -25,11 +25,10 @@
 
         <!-- Page Content -->
         <!-- Borrowing form grid -->              
-        <div class="mb-4">
-          <h3 class="display-10">Borrow Page</h3>
-        <form>
-          <!--id NUMBER-->
-          <div class="input-group mb-3">
+        
+      <form action="">
+              <!--id NUMBER-->
+              <div class="input-group mb-3">
             <input type="text" class="form-control" id="inputIDnum" placeholder="ID number of Student or Faculty" aria-label="Recipient's username" aria-describedby="button-addon2">
             <div class="input-group-append">
             <button class="btn btn-primary" type="button" id="button-search">
@@ -49,91 +48,52 @@
               <label for="inputFname">First Name</label>
               <input type="text" class="form-control" id="inputFname" placeholder="First Name" disabled>
             </div>
-          </div>
-          
-          <div class="form-row">
+        </div>
+
+        <div class="form-row">
             <!-- ContactNum input -->
             <div class="form-group col-md-6">
               <label for="inputContactnum">Contact Number</label>
               <input type="text" class="form-control" id="inputFname" placeholder="+63XXXXXXXXXX" disabled>
             </div>
-            <!-- LastName input -->
+            <!-- Date input -->
             <div class="form-group col-md-6">
               <label for="Datenow"> Date</label>
               <input type="text" id="Datenow" class="form-control" disabled>
-            </div>
-          </div>
+            </div>          
+        </div>
 
-          <!--Grid row Item Borrowed/Quantity-->
-          <div class="form-row mb-4">
+        <div class="form-group mb-4">
           <h3 class="display-10">Item Borrowed</h3>
           </div>
 
-          <div class="form-row mb-3"> 
-            <div class="input-group-prepend ">
-              <select type="text" class="form-control col-md-10" id="inputDept_Coll">
+          <div class="form-row">
+            <div class="form-group col-md-4">
+            <select type="text" class="form-control" id="inputDept_Coll">
                 <option disabled selected>Items</option>
                 <option> test Items</option>
-              </select>
-              <input type="text" class="form-control col-md-4" id="inputQuantity" placeholder="Quantity">              
-              <input type="text" class="form-control col-md-12" id="inputRemarks" placeholder="Remarks">
-            </div>
-
-          </div>
-
-          <!-- Grid row  Buttons -->          
-          <div class="btn-group">
-            <button type="submit" class="btn btn-primary btn-lg">Borrow</button>
-            <button type="clear" class="btn btn-primary btn-lg">Clear</button>
-          </div>
+            </select>
         
-        </form>
-      
-      </div>
+            </div>
 
-        <!-- DataTables Example -->
-        <div class="card mb-3">
-          <div class="card-header">
-            <i class="fas fa-table"></i>
-            Borrow Table</div>
-          <div class="card-body">
-            <div class="table-responsive">
-              <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                <thead>
-                  <tr>
-                    <th>Name</th>
-                    <th>Position</th>
-                    <th>Office</th>
-                    <th>Age</th>
-                    <th>Start date</th>
-                    <th>Salary</th>
-                  </tr>
-                </thead>
-                <tfoot>
-                  <tr>
-                    <th>Name</th>
-                    <th>Position</th>
-                    <th>Office</th>
-                    <th>Age</th>
-                    <th>Start date</th>
-                    <th>Salary</th>
-                  </tr>
-                </tfoot>
-                <tbody>
-                  <tr>
-                    <td>Donna Snider</td>
-                    <td>Customer Support</td>
-                    <td>New York</td>
-                    <td>27</td>
-                    <td>2011/01/25</td>
-                    <td>$112,000</td>
-                  </tr>
-                </tbody>
-              </table>
+            <div class="form-group col-md-2">
+              <input type="text" class="form-control" id="inputQuantity" placeholder="Quantity">
+            </div>
+            
+            <div class="form-group col-md-6">
+              <input type="text" class="form-control" id="inputRemarks" placeholder="Remarks">
+            </div>          
+          </div>
+          
+          <div class="form-group mb-4">
+            <div class="btn-group">
+              <button type="submit" class="btn btn-primary btn-lg">Borrow</button>
+              <button type="clear" class="btn btn-primary btn-lg">Clear</button>
             </div>
           </div>
-          <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
-        </div>
+      </form>
+
+
 
     </div><!-- /.container-fluid -->
   </div><!-- /.content-wrapper -->
--- return.php
+++ return.php
@@ -24,11 +24,7 @@
         </ol>
 
         <!-- Page Content -->
-        <h1 class="display-1">404</h1>
-        <p class="lead">Page not found. You can
-          <a href="javascript:history.back()">go back</a>
-          to the previous page, or
-          <a href="index.php">return home</a>.</p>
+
 
     </div><!-- /.container-fluid -->
   </div><!-- /.content-wrapper -->
