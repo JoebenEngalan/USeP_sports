6392400c0c17371586a9261528cb0a6ad64fa818
trone1993@gmail.com <trone1993@gmail.com>
2019-07-02 10:00:34 +0800

    commit sidebar

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -26,6 +26,26 @@
         </div>
       </li>
       <li class="nav-item">
+        <a class="nav-link" href="add-borrower.php">
+          <i class="fas fa-fw fa-male"></i>
+          <span>Add Borrowers Page</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="borrow.php">
+          <i class="fas fa-fw fa-redo"></i>
+          <span>Borrow Page</span></a>
+      </li> 
+      <li class="nav-item">
+        <a class="nav-link" href="return.php">
+          <i class="fas fa-fw fa-undo"></i>
+          <span>Return Page</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="add-item.php">
+          <i class="fas fa-fw fa-bicycle"></i>
+          <span>Add Items</span></a>
+      </li>
+      <li class="nav-item">
         <a class="nav-link" href="tables.php">
           <i class="fas fa-fw fa-table"></i>
           <span>Tables</span></a>
