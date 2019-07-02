43f0bb74daeb9842f54c1f77c001f52ed6a22b54
trone1993@gmail.com <trone1993@gmail.com>
2019-07-02 10:04:07 +0800

    commit side

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -7,24 +7,6 @@
           <span>Dashboard</span>
         </a>
       </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          <i class="fas fa-fw fa-folder"></i>
-          <span>Pages</span>
-        </a>
-        <div class="dropdown-menu" aria-labelledby="pagesDropdown">
-          <h6 class="dropdown-header">Login Screens:</h6>
-          <a class="dropdown-item" href="login.php">Login</a>
-          <a class="dropdown-item" href="register.php">Register</a>
-          <a class="dropdown-item" href="forgot-password.php">Forgot Password</a>
-          <div class="dropdown-divider"></div>
-          <h6 class="dropdown-header">Transactions Page:</h6>
-          <a class="dropdown-item" href="add-borrower.php">Add Borrowers Page</a>
-          <a class="dropdown-item" href="borrow.php">Borrow Page</a>
-          <a class="dropdown-item" href="return.php">Return Page</a>
-          <a class="dropdown-item" href="add-item.php">Add Items</a>
-        </div>
-      </li>
       <li class="nav-item">
         <a class="nav-link" href="add-borrower.php">
           <i class="fas fa-fw fa-male"></i>
