1f5a0422f9d4788af3339f1bf68f2f777caf1ad4
trone1993@gmail.com <trone1993@gmail.com>
2019-07-03 15:29:26 +0800

    commit change

--- index.php
+++ index.php
@@ -31,23 +31,7 @@ echo "<script type='text/javascript'> document.location = 'mainpage.php'; </scri
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
-
-  <title>SB Admin - Login</title>
-
-  <!-- Custom fonts for this template-->
-  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-
-  <!-- Custom styles for this template-->
-  <link href="css/sb-admin.css" rel="stylesheet">
-
-</head>
+<?php include('templates/head.php');?>
 
 <body class="bg-dark">
 
@@ -87,12 +71,7 @@ echo "<script type='text/javascript'> document.location = 'mainpage.php'; </scri
     </div>
   </div>
 
-  <!-- Bootstrap core JavaScript-->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-  <!-- Core plugin JavaScript-->
-  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+  <?php include('templates/scripts.php')?>
 
 </body>
 
