7c890f747225142a020373371c823a4dc9a3b864
trone1993@gmail.com <trone1993@gmail.com>
2019-07-04 08:20:39 +0800

    commit all change

--- includes/borrowingsystem.sql
+++ includes/borrowingsystem.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 4.8.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 28, 2019 at 04:53 AM
--- Server version: 10.1.40-MariaDB
--- PHP Version: 7.3.5
+-- Generation Time: Jul 03, 2019 at 09:44 AM
+-- Server version: 10.1.34-MariaDB
+-- PHP Version: 7.2.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -49,23 +49,60 @@ INSERT INTO `admin` (`admin_ID`, `Name`, `Password`, `Datelogs`) VALUES
 --
 
 CREATE TABLE `borrower_table` (
-  `Borrower_ID` int(100) NOT NULL,
+  `id` int(11) NOT NULL,
+  `id_number` varchar(50) NOT NULL,
   `FirstName` varchar(100) DEFAULT NULL,
   `LastName` varchar(100) DEFAULT NULL,
-  `ContactNumber` int(50) DEFAULT NULL,
+  `ContactNumber` varchar(50) DEFAULT NULL,
   `Department` varchar(50) DEFAULT NULL,
   `Position` varchar(50) DEFAULT NULL,
-  `EmailID` varchar(50) NOT NULL
+  `EmailID` varchar(50) NOT NULL,
+  `borrow` datetime NOT NULL,
+  `Status` int(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `borrower_table`
+--
+
+INSERT INTO `borrower_table` (`id`, `id_number`, `FirstName`, `LastName`, `ContactNumber`, `Department`, `Position`, `EmailID`, `borrow`, `Status`) VALUES
+(1, '2014', 'shai', 'celer', '2147483647', NULL, NULL, 'shai@gmail.com', '0000-00-00 00:00:00', 1),
+(2, '2014-15358', 'shaira', 'celerian', '2147483647', NULL, NULL, 'shaiar@gmail.com', '0000-00-00 00:00:00', 1),
+(3, '2014-6565685', 'simplyn', 'madula', '2147483647', NULL, NULL, 'madula@gmail.com', '0000-00-00 00:00:00', 1),
+(4, '2014-15358', 'wattap', 'hey', '2147483647', 'College of Education', 'Student', 'hey@gmail.com', '0000-00-00 00:00:00', 1),
+(5, '2014-15358', 'shaira', 'celerian', '2147483647', 'Institute of Computing', 'Student', 'shairacelerian@gmail.com', '0000-00-00 00:00:00', 1),
+(6, '2014-19596', 'shaira', 'celerian', '2147483647', 'Institute of Computing', 'Faculty and Staff', 'hey@gmail.com', '0000-00-00 00:00:00', 1),
+(7, '2014-19596', 'shaira', 'celerian', '09105824423', 'Institute of Computing', 'Faculty and Staff', 'hey@gmail.com', '0000-00-00 00:00:00', 1),
+(8, '2014-15358', 'haloo', 'heloo', '09105824423', NULL, NULL, 'halo@gmail.com', '0000-00-00 00:00:00', 1),
+(9, '2014-15358', 'sup', 'wat', '09154845552', 'College of Engineering', 'Faculty and Staff', 'watsup@gmail.com', '0000-00-00 00:00:00', 1),
+(10, '2014-65986', 'low', 'hey', '09105824422', 'Institute of Computing', 'Faculty and Staff', 'yea@gmail.com', '0000-00-00 00:00:00', 1),
+(11, '2014-15358', 'sure', 'huyy', '09105824423', 'College of Engineering', 'Student', 'huy@gmail.com', '0000-00-00 00:00:00', 1),
+(12, '2014-15358', 'low', 'hey', '09610582442', 'School Applied Economics', 'Faculty and Staff', 'low@gmail.com', '0000-00-00 00:00:00', 1),
+(13, '2014-15358', 'low', 'hey', '09610582442', 'School Applied Economics', 'Faculty and Staff', 'low@gmail.com', '0000-00-00 00:00:00', 1),
+(14, '2014-15358', 'madula', 'hey', '09105824422', 'Institute of Computing', 'Student', 'madula@gmail.com', '0000-00-00 00:00:00', 1),
+(15, '2015-15668', 'lala', 'lolo', '09105824423', 'School Applied Economics', 'Faculty and Staff', 'lolo@gmail.com', '0000-00-00 00:00:00', 1),
+(16, '25896-698565656', 'simplyn', 'madula', '09105824223', 'College of Technology', 'Faculty and Staff', 'madulasimplyn@gmail.com', '0000-00-00 00:00:00', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `borrowpage`
+--
+
+CREATE TABLE `borrowpage` (
+  `Item_borrowed` varchar(150) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `BorrowPage`
+-- Table structure for table `equipment`
 --
 
-CREATE TABLE `BorrowPage` (
-  `Item_borrowed` varchar(150) NOT NULL
+CREATE TABLE `equipment` (
+  `id` int(11) NOT NULL,
+  `Type` varchar(100) NOT NULL,
+  `Description` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -89,7 +126,15 @@ CREATE TABLE `staff_table` (
 
 INSERT INTO `staff_table` (`id`, `FirstName`, `LastName`, `Email`, `Password`, `ContactNumber`) VALUES
 (1, 'shaira', 'celerian', 'shai@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 0),
-(2, 'simplyn', 'madula', 'madula@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 0);
+(2, 'simplyn', 'madula', 'madula@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 0),
+(3, NULL, NULL, NULL, 'd41d8cd98f00b204e9800998ecf8427e', 0),
+(4, NULL, NULL, NULL, 'd41d8cd98f00b204e9800998ecf8427e', 0),
+(5, NULL, NULL, NULL, 'd41d8cd98f00b204e9800998ecf8427e', 0),
+(6, NULL, NULL, NULL, 'd41d8cd98f00b204e9800998ecf8427e', 0),
+(7, NULL, NULL, NULL, 'd41d8cd98f00b204e9800998ecf8427e', 0),
+(8, NULL, NULL, NULL, 'd41d8cd98f00b204e9800998ecf8427e', 0),
+(9, NULL, NULL, NULL, 'd41d8cd98f00b204e9800998ecf8427e', 0),
+(10, 'lala', 'lolo', 'lolo@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 0);
 
 --
 -- Indexes for dumped tables
@@ -105,7 +150,13 @@ ALTER TABLE `admin`
 -- Indexes for table `borrower_table`
 --
 ALTER TABLE `borrower_table`
-  ADD PRIMARY KEY (`Borrower_ID`);
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `equipment`
+--
+ALTER TABLE `equipment`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `staff_table`
@@ -124,10 +175,22 @@ ALTER TABLE `admin`
   MODIFY `admin_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT for table `borrower_table`
+--
+ALTER TABLE `borrower_table`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT for table `equipment`
+--
+ALTER TABLE `equipment`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `staff_table`
 --
 ALTER TABLE `staff_table`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- index.php
+++ index.php
@@ -31,7 +31,23 @@ echo "<script type='text/javascript'> document.location = 'mainpage.php'; </scri
 <!DOCTYPE html>
 <html lang="en">
 
-<?php include('templates/head.php');?>
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>SB Admin - Login</title>
+
+  <!-- Custom fonts for this template-->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+
+  <!-- Custom styles for this template-->
+  <link href="css/sb-admin.css" rel="stylesheet">
+
+</head>
 
 <body class="bg-dark">
 
@@ -71,7 +87,12 @@ echo "<script type='text/javascript'> document.location = 'mainpage.php'; </scri
     </div>
   </div>
 
-  <?php include('templates/scripts.php')?>
+  <!-- Bootstrap core JavaScript-->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
 
 </body>
 
