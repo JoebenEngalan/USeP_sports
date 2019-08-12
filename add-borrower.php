bb21cd29bcccccf8ba375709ca9616d37dd99b9e
tron1993 <trone1993@gmail.com>
2019-08-12 14:38:28 +0800

    chnage

--- add-borrower.php
+++ add-borrower.php
@@ -121,11 +121,12 @@ if(!isset($_SESSION["id"]))
 
 </body>
 
-</html>
-
 <!--no form resubmision javascript dont touch-->
 <script>
   if ( window.history.replaceState ) {
     window.history.replaceState( null, null, window.location.href );
   }
 </script>
+
+</html>
+
