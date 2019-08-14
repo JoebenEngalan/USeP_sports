668da33562651b861bae1dc658cdc9ede0db4113
trone1993@gmail.com <trone1993@gmail.com>
2019-08-14 09:41:13 +0800

    commit change

--- templates/getitem.php
+++ templates/getitem.php
@@ -1,5 +1,4 @@
 <?php
-
 // set data in input text
 $itemname = "";
 $quantity = "";
@@ -7,26 +6,22 @@ if(isset($_POST['getitem']))
 {
 // id to search
 $itemname = $_POST['itemname1'];
-
 // mysql search query  
 $sql = "SELECT * FROM equipment  WHERE ItemName = :itemname1";  
 $pdoResult = $dbh->prepare($sql);  
 //set your id to the query id
 $pdoExec = $pdoResult->execute(array(":itemname1"=>$itemname));
                 
-if($pdoExec)
-{
-    if($pdoResult->rowCount()>0)
+    if($pdoExec)
     {
-    foreach($pdoResult as $row)
-    {   
-        $itemname = $row['ItemName'];
-        $quantity = $row['quantity'];
-       
-    }
-    }
+        if($pdoResult->rowCount()>0)
+        {
+            foreach($pdoResult as $row)
+            {   
+                $itemname = $row['ItemName'];
+                $quantity = $row['quantity'];                
+            }
+        }
     }
 } 
-
-
 ?>
