3f4ac904bc2ef796e4d78636ecb0d1c1d575a126
tron1993 <trone1993@gmail.com>
2019-09-11 18:12:33 +0800

    commit change

--- templates/borrowitems.php
+++ templates/borrowitems.php
@@ -4,9 +4,6 @@
 if(isset($_POST['submit']))
 {
 $id_number=$_POST["id_number"];
-$FirstName=$_POST["FirstName"];
-$LastName=$_POST["LastName"];
-$ContactNumber=$_POST["ContactNumber"];
 $item1=$_POST['1itemname'];
 $quantity1=$_POST['quantity1'];
 $item2=$_POST['2itemname'];
@@ -19,18 +16,12 @@ $remarks=$_POST['remarks'];
 $Btime=$_POST['Btime'];
 $status=1;
 
-$sql= "INSERT INTO Borrowed_Item (id_number,FirstName,LastName,ContactNumber,Item,quantity,Remarks,Borrowed_time,status) 
+$sql= "INSERT INTO Borrowed_Item (id_number,Item1,quantity1,Item2,quantity2,Item3,quantity3,Item4,quantity4,Remarks,Borrowed_time,status) 
        VALUES
-        (:id_number,:FirstName,:LastName,:ContactNumber,:1itemname,:quantity1,:remarks,:Btime,:status),
-        (:id_number,:FirstName,:LastName,:ContactNumber,:2itemname,:quantity2,:remarks,:Btime,:status),
-        (:id_number,:FirstName,:LastName,:ContactNumber,:3itemname,:quantity3,:remarks,:Btime,:status),
-        (:id_number,:FirstName,:LastName,:ContactNumber,:4itemname,:quantity4,:remarks,:Btime,:status)";
+        (:id_number,:1itemname,:quantity1,:2itemname,:quantity2,:3itemname,:quantity3,:4itemname,:quantity4,:remarks,:Btime,:status)";
 
 $query=$dbh->prepare($sql);
 $query->bindParam(':id_number',$id_number,PDO::PARAM_STR);
-$query->bindParam(':FirstName',$FirstName,PDO::PARAM_STR);
-$query->bindParam(':LastName',$LastName,PDO::PARAM_STR);
-$query->bindParam(':ContactNumber',$ContactNumber,PDO::PARAM_STR);
 $query->bindParam(':remarks',$remarks,PDO::PARAM_STR);
 $query->bindParam(':1itemname',$item1,PDO::PARAM_STR);
 $query->bindParam(':quantity1',$quantity1,PDO::PARAM_STR);
