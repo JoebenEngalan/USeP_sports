1b43016a5e8b50f766eec11cdd6bf2349096662b
tron1993 <trone1993@gmail.com>
2019-09-16 17:14:44 +0800

    commit change

--- templates/addborrower.php
+++ templates/addborrower.php
@@ -4,40 +4,41 @@ include('includes/config.php');
 if(isset($_POST['submit']))
 
 {
-        $idnum=$_POST['idnumber'];
-        $fname=$_POST['firstname'];
-        $lname=$_POST['lastname'];
-        $contact=$_POST['contactnum'];
-        $depart=$_POST['department'];
-        $pos=$_POST['position'];
-        $email=$_POST['emailid'];
-        $status=1;
+  $idnum=$_POST['idnumber'];
+  $fname=$_POST['firstname'];
+  $lname=$_POST['lastname'];
+  $contact=$_POST['contactnum'];
+  $depart=$_POST['department'];
+  $pos=$_POST['position'];
+  $email=$_POST['emailid'];
+  $status=1;
 
-        if( empty($idnum) || empty($fname) || empty($lname) || 
-            empty($contact) || empty($depart) || empty($pos) || empty($email) )
-        {
-          {echo "<script type= 'text/javascript'>alert('Empty Fields.');</script>";}  
-        }
-        else
-        {
-        $sql= "INSERT INTO borrower_table (id_number,FirstName,LastName,ContactNumber,Department,Position,EmailID,Status) 
-                VALUES(:idnum,:fname,:lname,:contact,:depart,:pos,:email,:status)";
+  if( empty($idnum) || empty($fname) || empty($lname) || empty($contact) || empty($depart) || empty($pos) || empty($email) )
+  {
+  echo "<script type= 'text/javascript'>alert('Empty Fields.');</script>";  
+  }
+  else
+  {
+    $sql= "INSERT INTO borrower_table (id_number,FirstName,LastName,ContactNumber,Department,Position,EmailID,Status) 
+        VALUES(:idnum,:fname,:lname,:contact,:depart,:pos,:email,:status)";
 
-        $query = $dbh->prepare($sql);
-        $query->bindParam(':idnum',$idnum,PDO::PARAM_STR);
-        $query->bindParam(':fname',$fname,PDO::PARAM_STR);
-        $query->bindParam(':lname',$lname,PDO::PARAM_STR);
-        $query->bindParam(':contact',$contact,PDO::PARAM_STR);
-        $query->bindParam(':depart',$depart,PDO::PARAM_STR);
-        $query->bindParam(':pos',$pos,PDO::PARAM_STR);
-        $query->bindParam(':email',$email,PDO::PARAM_STR);
-        $query->bindParam(':status',$status,PDO::PARAM_STR);
-        $query->execute();
-        $lastInsertId = $dbh->lastInsertId();
-        if($lastInsertId)
-                {echo "<script type= 'text/javascript'>alert('New Record Inserted Successfully');</script>";}
-        else 
-                {echo "<script type= 'text/javascript'>alert('Data not successfully Inserted.');</script>";}
-        }        
+    $query = $dbh->prepare($sql);
+    $query->bindParam(':idnum',$idnum,PDO::PARAM_STR);
+    $query->bindParam(':fname',$fname,PDO::PARAM_STR);
+    $query->bindParam(':lname',$lname,PDO::PARAM_STR);
+    $query->bindParam(':contact',$contact,PDO::PARAM_STR);
+    $query->bindParam(':depart',$depart,PDO::PARAM_STR);
+    $query->bindParam(':pos',$pos,PDO::PARAM_STR);
+    $query->bindParam(':email',$email,PDO::PARAM_STR);
+    $query->bindParam(':status',$status,PDO::PARAM_STR);
+    $query->execute();
+    $lastInsertId = $dbh->lastInsertId();
+    if($lastInsertId)
+    {
+      echo "<script type= 'text/javascript'>alert('New Record Inserted Successfully');</script>";
+    }else{
+      echo "<script type= 'text/javascript'>alert('Data not successfully Inserted.');</script>";
+    }
+  }        
 }
 ?>
