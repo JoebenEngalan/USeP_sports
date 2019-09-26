e9b2bfd06126f2eaf1a39ac138bcb4256ca82633
tron1993 <trone1993@gmail.com>
2019-09-26 16:53:55 +0800

    commit changes

--- templates/registeruser.php
+++ templates/registeruser.php
@@ -10,28 +10,28 @@ $email=$_POST['emailid'];
 $password=md5($_POST['password']);
 
 if( empty($fname) || empty($lname) || empty($email) || empty($password))
+  {
+  echo "<script type= 'text/javascript'>alert('Empty Fields.');</script>";  
+  }
+  else
+  {
+  $sql= "INSERT INTO staff_table (FirstName,LastName,Email,Password)
+  VALUES(:fname,:lname,:email,:password)";
+  $query = $dbh->prepare($sql);
+  $query->bindParam(':fname',$fname,PDO::PARAM_STR);
+  $query->bindParam(':lname',$lname,PDO::PARAM_STR);
+  $query->bindParam(':email',$email,PDO::PARAM_STR);
+  $query->bindParam(':password',$password,PDO::PARAM_STR);
+  $query->execute();
+  $lastInsertId = $dbh->lastInsertId();
+  if($lastInsertId)
     {
-    echo "<script type= 'text/javascript'>alert('Empty Fields.');</script>";  
+      echo "<script>alert('Registration successfull. Now you can login');</script>";
     }
-    else
+    else 
     {
-    $sql= "INSERT INTO staff_table (FirstName,LastName,Email,Password) VALUES(:fname,:lname,:email,:password)";
-    $query = $dbh->prepare($sql);
-    $query->bindParam(':fname',$fname,PDO::PARAM_STR);
-    $query->bindParam(':lname',$lname,PDO::PARAM_STR);
-    $query->bindParam(':email',$email,PDO::PARAM_STR);
-    $query->bindParam(':password',$password,PDO::PARAM_STR);
-    $query->execute();
-    $lastInsertId = $dbh->lastInsertId();
-    if($lastInsertId)
-        {
-            echo "<script>alert('Registration successfull. Now you can login');</script>";
-        }
-        else 
-        {
-            echo "<script>alert('Something went wrong. Please try again');</script>";
-        }
-
-    }    
+      echo "<script>alert('Something went wrong. Please try again');</script>";
+    }
+  }    
 }
 ?>
