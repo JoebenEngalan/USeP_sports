0d02d3d09b1bdce2f6db5e0310de5a37087df3ca
tron1993 <trone1993@gmail.com>
2019-09-26 16:44:33 +0800

    commit change

--- templates/additem.php
+++ templates/additem.php
@@ -2,7 +2,7 @@
 include('includes/config.php');
 
 if(isset($_POST['submit']))
-  {    
+{    
   $itemname=$_POST['itemname'];
   $description=$_POST['description'];
   $category=$_POST['category'];
@@ -16,27 +16,28 @@ if(isset($_POST['submit']))
   else
   {
   
-    $sql= "INSERT INTO equipment (ItemName,Description,Category,quantity,status) 
-            VALUES(:itemname,:description,:category,:quantity,:status)";
+  $sql= "INSERT INTO equipment (ItemName,Description,Category,quantity,status) 
+        VALUES(:itemname,:description,:category,:quantity,:status)";
 
-    $query = $dbh->prepare($sql);
+  $query = $dbh->prepare($sql);
 
-    $query->bindParam(':itemname',$itemname,PDO::PARAM_STR);
-    $query->bindParam(':description',$description,PDO::PARAM_STR);
-    $query->bindParam(':category',$category,PDO::PARAM_STR);
-    $query->bindParam(':quantity',$quantity,PDO::PARAM_STR);
-    $query->bindParam(':status',$status,PDO::PARAM_STR);
-    $query->execute();
-    $lastInsertId = $dbh->lastInsertId();
-    if($lastInsertId)
-      {
-      echo "<script>alert('You successfully added an item!');</script>";
-      
-      }
-      else 
-      {
-      echo "<script>alert('Something went wrong. Please try again');</script>";
-      }
+  $query->bindParam(':itemname',$itemname,PDO::PARAM_STR);
+  $query->bindParam(':description',$description,PDO::PARAM_STR);
+  $query->bindParam(':category',$category,PDO::PARAM_STR);
+  $query->bindParam(':quantity',$quantity,PDO::PARAM_STR);
+  $query->bindParam(':status',$status,PDO::PARAM_STR);
+  $query->execute();
+  $lastInsertId = $dbh->lastInsertId();
+
+  if($lastInsertId)
+    {
+    echo "<script>alert('You successfully added an item!');</script>";   
+    }
+    else 
+    {
+    echo "<script>alert('Something went wrong. Please try again');</script>";
     }
   }
+}
+
 ?>
