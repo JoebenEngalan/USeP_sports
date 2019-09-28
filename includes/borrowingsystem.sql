91a99bcf99567d8f8c90c2a039abf818c54bfba9
tron1993 <trone1993@gmail.com>
2019-09-29 01:36:30 +0800

    commit database

--- includes/borrowingsystem.sql
+++ includes/borrowingsystem.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 26, 2019 at 10:42 AM
+-- Generation Time: Sep 28, 2019 at 07:35 PM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.3.1
 
@@ -66,25 +66,9 @@ CREATE TABLE `borrower_table` (
 --
 
 INSERT INTO `borrower_table` (`id`, `id_number`, `FirstName`, `LastName`, `ContactNumber`, `Department`, `Position`, `EmailID`) VALUES
-(1, '2014', 'shai', 'celer', '2147483647', NULL, NULL, 'shai@gmail.com'),
-(2, '2014-15358', 'shaira', 'celerian', '2147483647', NULL, NULL, 'shaiar@gmail.com'),
-(3, '2014-6565685', 'simplyn', 'madula', '2147483647', NULL, NULL, 'madula@gmail.com'),
-(4, '2014-15358', 'wattap', 'hey', '2147483647', 'College of Education', 'Student', 'hey@gmail.com'),
-(5, '2014-15358', 'shaira', 'celerian', '2147483647', 'Institute of Computing', 'Student', 'shairacelerian@gmail.com'),
-(6, '2014-19596', 'shaira', 'celerian', '2147483647', 'Institute of Computing', 'Faculty and Staff', 'hey@gmail.com'),
-(7, '2014-19596', 'shaira', 'celerian', '09105824423', 'Institute of Computing', 'Faculty and Staff', 'hey@gmail.com'),
-(8, '2014-153', 'haloo', 'heloo', '09105824423', NULL, NULL, 'halo@gmail.com'),
-(9, '2014-15358', 'sup', 'wat', '09154845552', 'College of Engineering', 'Faculty and Staff', 'watsup@gmail.com'),
-(10, '2014-65986', 'low', 'hey', '09105824422', 'Institute of Computing', 'Faculty and Staff', 'yea@gmail.com'),
-(11, '2014-15358', 'sure', 'huyy', '09105824423', 'College of Engineering', 'Student', 'huy@gmail.com'),
-(12, '2014-15358', 'low', 'hey', '09610582442', 'School Applied Economics', 'Faculty and Staff', 'low@gmail.com'),
-(13, '2014-15358', 'low', 'hey', '09610582442', 'School Applied Economics', 'Faculty and Staff', 'low@gmail.com'),
-(14, '2014-15358', 'madula', 'hey', '09105824422', 'Institute of Computing', 'Student', 'madula@gmail.com'),
-(15, '2015-15668', 'lala', 'lolo', '09105824423', 'School Applied Economics', 'Faculty and Staff', 'lolo@gmail.com'),
-(16, '25896-698565656', 'simplyn', 'madula', '09105824223', 'College of Technology', 'Faculty and Staff', 'madulasimplyn@gmail.com'),
-(17, '2014-15365598', 'shaira', 'celerian', '09105824423', 'Institute of Computing', 'Faculty and Staff', 'dsdsdd@gmail.com'),
-(18, '2014-15358', 'cccc', 'dddd', '09105824423', 'College of Engineering', 'Student', 'cc@gmail.com'),
-(19, '2014-12222', 'test', 'wq', '09728127192', 'College of Arts and Sciences', 'Faculty and Staff', 'test@gmail.com');
+(1, '2014-00000', 'shai', 'celer', '2147483647', 'College of Engineering', 'Student', 'shai@gmail.com'),
+(2, '2014-15358', 'shaira', 'celerian', '2147483647', 'College of Engineering', 'Student', 'shaiar@gmail.com'),
+(3, '2014-11111', 'test', 'test', '09864914109', 'College of Engineering', 'Student', 'email@email.com');
 
 -- --------------------------------------------------------
 
@@ -97,21 +81,21 @@ CREATE TABLE `equipment` (
   `ItemName` varchar(500) DEFAULT NULL,
   `Description` text,
   `Category` varchar(50) DEFAULT NULL,
-  `quantity` int(100) DEFAULT NULL,
-  `status` int(1) DEFAULT NULL
+  `quantity` int(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `equipment`
 --
 
-INSERT INTO `equipment` (`id`, `ItemName`, `Description`, `Category`, `quantity`, `status`) VALUES
-(3, 'test', 'ffffffffffffffffffff', 'Sports Equipment', 28, 1),
-(4, 'test2', 'deeeeeeeeeeeeeeeeeeeeeeee', 'Sports Equipment', 28, 1),
-(5, 'test3', 'deeeeeeeeeeeeeeeeeeeeeeee', 'Sports Equipment', 7, 1),
-(6, 'test4', 'dfedfdfggf', 'Gym Equipment', 10, 1),
-(20, 'GeeksForGeeks', 'Make', 'Sports Equipment', 1, 1),
-(21, 'help', 'test', 'Sports Equipment', 21, 1);
+INSERT INTO `equipment` (`id`, `ItemName`, `Description`, `Category`, `quantity`) VALUES
+(3, 'test', 'ffffffffffffffffffff', 'Sports Equipment', 28),
+(4, 'test2', 'deeeeeeeeeeeeeeeeeeeeeeee', 'Sports Equipment', 28),
+(5, 'test3', 'deeeeeeeeeeeeeeeeeeeeeeee', 'Sports Equipment', 7),
+(6, 'test4', 'dfedfdfggf', 'Gym Equipment', 10),
+(20, 'GeeksForGeeks', 'Make', 'Sports Equipment', 1),
+(21, 'help', 'test', 'Sports Equipment', 21),
+(22, 'help2', 'test', 'Sports Equipment', 40);
 
 -- --------------------------------------------------------
 
@@ -135,15 +119,6 @@ CREATE TABLE `returned_item` (
   `quantity4` int(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `returned_item`
---
-
-INSERT INTO `returned_item` (`ID`, `id_number`, `ContactNumber`, `Borrowed_time`, `Return_time`, `Item1`, `quantity1`, `Item2`, `quantity2`, `Item3`, `quantity3`, `Item4`, `quantity4`) VALUES
-(1, '2014', 2147483647, '9/17/2019', '9/26/2019', 'test', 7, 'test2', 7, '', 0, '', 0),
-(2, '2014', 2147483647, '9/17/2019', '9/26/2019', 'test', 7, 'test2', 7, '', 0, '', 0),
-(3, '2014', 2147483647, '9/26/2019', '9/26/2019', 'test', 8, 'test2', 8, '', 0, '', 0);
-
 -- --------------------------------------------------------
 
 --
@@ -155,28 +130,15 @@ CREATE TABLE `staff_table` (
   `FirstName` varchar(100) DEFAULT NULL,
   `LastName` varchar(100) DEFAULT NULL,
   `Email` varchar(100) DEFAULT NULL,
-  `Password` varchar(100) DEFAULT NULL,
-  `ContactNumber` int(50) NOT NULL,
-  `date_added` datetime NOT NULL,
-  `date_updated` datetime DEFAULT NULL
+  `Password` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `staff_table`
 --
 
-INSERT INTO `staff_table` (`id`, `FirstName`, `LastName`, `Email`, `Password`, `ContactNumber`, `date_added`, `date_updated`) VALUES
-(1, 'shaira', 'celerian', 'shai@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 0, '2019-07-04 08:59:45', '2019-07-04 09:00:31'),
-(2, 'simplyn', 'madula', 'madula@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 0, '2019-07-04 08:59:45', '2019-07-04 09:00:31'),
-(3, NULL, NULL, NULL, 'd41d8cd98f00b204e9800998ecf8427e', 0, '2019-07-04 08:59:45', '2019-07-04 09:00:31'),
-(4, NULL, NULL, NULL, 'd41d8cd98f00b204e9800998ecf8427e', 0, '2019-07-04 08:59:45', '2019-07-04 09:00:31'),
-(5, NULL, NULL, NULL, 'd41d8cd98f00b204e9800998ecf8427e', 0, '2019-07-04 08:59:45', '2019-07-04 09:00:31'),
-(6, NULL, NULL, NULL, 'd41d8cd98f00b204e9800998ecf8427e', 0, '2019-07-04 08:59:45', '2019-07-04 09:00:31'),
-(7, NULL, NULL, NULL, 'd41d8cd98f00b204e9800998ecf8427e', 0, '2019-07-04 08:59:45', '2019-07-04 09:00:31'),
-(8, NULL, NULL, NULL, 'd41d8cd98f00b204e9800998ecf8427e', 0, '2019-07-04 08:59:45', '2019-07-04 09:00:31'),
-(9, NULL, NULL, NULL, 'd41d8cd98f00b204e9800998ecf8427e', 0, '2019-07-04 08:59:45', '2019-07-04 09:00:31'),
-(10, 'lala', 'lolo', 'lolo@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 0, '2019-07-04 08:59:45', '2019-07-04 09:00:31'),
-(11, 'testhere', 'test', 'Test@gmail.com', 'fcea920f7412b5da7be0cf42b8c93759', 0, '0000-00-00 00:00:00', NULL);
+INSERT INTO `staff_table` (`id`, `FirstName`, `LastName`, `Email`, `Password`) VALUES
+(1, 'Admin01F', 'Admin01L', 'Admin@gmail.com', '0e7517141fb53f21ee439b355b5a1d0a');
 
 --
 -- Indexes for dumped tables
@@ -192,13 +154,15 @@ ALTER TABLE `borrowed_item`
 -- Indexes for table `borrower_table`
 --
 ALTER TABLE `borrower_table`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id_number` (`id_number`);
 
 --
 -- Indexes for table `equipment`
 --
 ALTER TABLE `equipment`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `ItemName` (`ItemName`);
 
 --
 -- Indexes for table `returned_item`
@@ -210,7 +174,8 @@ ALTER TABLE `returned_item`
 -- Indexes for table `staff_table`
 --
 ALTER TABLE `staff_table`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `Email` (`Email`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -220,31 +185,31 @@ ALTER TABLE `staff_table`
 -- AUTO_INCREMENT for table `borrowed_item`
 --
 ALTER TABLE `borrowed_item`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `borrower_table`
 --
 ALTER TABLE `borrower_table`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT for table `equipment`
 --
 ALTER TABLE `equipment`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT for table `returned_item`
 --
 ALTER TABLE `returned_item`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `staff_table`
 --
 ALTER TABLE `staff_table`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
