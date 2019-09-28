c07601474a0ee3b63769493f71983b7511e7149c
tron1993 <trone1993@gmail.com>
2019-09-29 00:28:45 +0800

    commit revisions

--- templates/footer.php
+++ templates/footer.php
@@ -7,9 +7,8 @@
       
     <div class="copyright text-center my-auto">
         <span> All rights Reserved. Develop for USeP Sports.<br></span><br>
-        <span>Contacts:</span>
-        <a href="https://github.com/JoebenEngalan/USeP_sports"><i class="fab fa-github"></i></a>
-        <a href="https://www.facebook.com/TroneThrone1993"><i class="fab fa-facebook"></i></a>
+        <a href="https://github.com/JoebenEngalan/USeP_sports"><i class="fab fa-github fa-2x"></i></a>
+        <a href="https://www.facebook.com/TroneThrone1993"><i class="fab fa-facebook fa-2x"></i></a>
     </div>
       
   </div>
