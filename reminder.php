450a87e5b8e8085e41e633902c1beafb6aeefe2f
tron1993 <trone1993@gmail.com>
2019-09-28 23:34:29 +0800

    commit change

--- reminder.php
+++ reminder.php
@@ -7,11 +7,11 @@
 
   <div class="container">
     <div class="card card-login mx-auto mt-5">
-      <div class="card-header">Reset Password</div>
+      <div class="card-header">Reminders</div>
       <div class="card-body">
         <div class="text-center mb-4">
-          <h4>Forgot your password?</h4>
-          <p>Please Contact the System Administrator to Reset your Password.</p>
+          <h4>Register an Account or Forgot your password?</h4>
+          <p>Please Contact the System Administrator to Register an Account or Reset your Password.</p>
         </div>
         <div class="text-center">
           <a class="d-block small mt-3" href="index.php">Login Page</a>
